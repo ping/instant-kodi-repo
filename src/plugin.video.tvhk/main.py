acd0cede8e8bf6dea1891753fb1a45f203394e7c
csvke <frankie.yuen@me.com>
2020-02-15 01:41:32 +0800

    bbc one hd url

--- src/plugin.video.tvhk/main.py
+++ src/plugin.video.tvhk/main.py
@@ -62,7 +62,7 @@ VIDEOS = [
     {
         'name': "BBC",
         'thumb': 'https://www.rthk.hk/assets/rthk/images/tv/player/500x281.jpg',
-        'video': 'https://raw.githubusercontent.com/m3u8playlist/dp/master/live_tv/bbc.m3u',
+        'video': 'http://a.files.bbci.co.uk/media/live/manifesto/audio_video/simulcast/hls/uk/abr_hdtv/ak/bbc_one_hd.m3u8',
     }
 ]
 
