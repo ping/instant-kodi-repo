0dde14e5bfd3cdd4811f37abcc576b946a2d79ea
csvke <frankie.yuen@me.com>
2020-02-20 17:07:04 +0800

    init commit for mpweekly

---
+++ src/plugin.video.mpweekly/resources/lib/addonutils.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import re
+from kodiutils import kodi_json_request
+
+def return_duration_as_seconds(string):
+    try:
+        totalseconds = 0
+        hours = re.findall(r'(\d+)H',string)
+        minutes = re.findall(r'(\d+)M',string)
+        seconds = re.findall(r'(\d+)S',string)
+        if hours:
+            totalseconds += 3600*int(hours[0])
+        if minutes:
+            totalseconds += 60*int(minutes[0])
+        if seconds:
+            totalseconds += int(seconds[0])
+        return str(totalseconds)
+    except IndexError:
+        return '0'
+
+def is_youtube_addon_installed():
+    result = kodi_json_request(
+        {
+            "jsonrpc": "2.0",
+            "method": "Addons.GetAddonDetails",
+            "params": {
+                "addonid": "plugin.video.youtube",
+                "properties": ["installed"]
+            },
+            "id": 11
+        }
+    )
+    return bool(result and result["addon"]["installed"])
